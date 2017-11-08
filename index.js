42a2a3f5bc3f1052a366f38b5ea7aaa8e5862f05
github username <github email address>
2017-11-07 22:42:00 -0500

    Done.

--- index.js
+++ index.js
@@ -1,22 +1,66 @@
-function getIssues() {
-}
+const token = getToken();
+const userName = 'Meeekks'
+const baseApi = 'https://api.github.com/'
 
-function showIssues(json) {
-}
-
-function createIssue() {
-}
-
-function showResults(json) {
+function getToken() {
+  return 'f19a76dfe60ce4b469e1a5e9345c550386bad0af'
 }
 
 function forkRepo() {
-  const repo = 'learn-co-curriculum/javascript-fetch-lab'
-  //use fetch to fork it!
+  const repo = "learn-co-curriculum/javascript-fetch-lab"
+  fetch(`${baseApi}repos/${repo}/forks`, {
+    method: "post",
+    headers: {
+      Authorization: `token ${token}`
+    }
+  })
+    .then(res => res.json())
+    .then(json => showForkedRepo(json));
 }
 
-function getToken() {
-  //change to your token to run in browser, but set
-  //back to '' before committing so all tests pass
-  return ''
+function showForkedRepo(json) {
+  const results = document.querySelector("#results");
+  const link = document.createElement("a");
+  link.href = json.html_url;
+  link.innerText = `Fork ${userName}'s Repository`;
+  results.append(link);
+}
+
+function createIssue() {
+  const repo = "/javascript-fetch-lab/issues"
+  const issue = {
+    title: document.querySelector("#title").value,
+    body: document.querySelector("#body").value
+  }
+  fetch(`${baseApi}repos/${userName}${repo}`, {
+    method: "post",
+    headers: {
+      Authorization: `token ${token}`
+    },
+    body: JSON.stringify(issue)
+  })
+    .then(res => res.json())
+    .then(json => getIssues(json));
+}
+
+function getIssues(json) {
+  const repo = "/javascript-fetch-lab/issues"
+  fetch(`${baseApi}repos/${userName}${repo}`, {
+    headers: {
+      Authorization: `token ${token}`
+    }
+  })
+    .then(res => res.json())
+    .then(json => showIssues(json))
+}
+
+function showIssues(json) {
+  const issues = document.getElementById("#issues");
+  return json.forEach(function(issue) {
+    const ul = document.createElement("ul");
+    const li = document.createElement("li");
+    li.innerText = issue.title;
+    ul.append(li);
+    return issues.append(ul);
+  });
 }
